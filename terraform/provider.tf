fb2898791cb61a54f1678434eae6c43ee1676410
Leonid Kozhaev <itserv2010@gmail.com>
2022-06-12 19:47:58 +0300

    edit provider iam key

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "yandex" {
 #  token     = var.yc_token
-  service_account_key_file = "iam/key.json"
+  service_account_key_file = "/iam/key.json"
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
   zone      = "ru-central1-a"
